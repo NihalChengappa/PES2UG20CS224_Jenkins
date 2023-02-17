ee27fda6b08d84ed62736fe2e10882e1674cafea
PES2UG20CS224_Nihal Chengappa P.A <87144286+NihalChengappa@users.noreply.github.com>
2023-02-17 11:22:22 +0530

    Create test.cpp

---
+++ main/test.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins webhook\n";
+  cout << "I have successfully built and run a jenkins job\n";
+  return 0;
+}
